811fbedfa7e8ddff5b371704bb9449b18d9c4199
Keunbae Park <pkboom@users.noreply.github.com>
2023-05-14 11:56:56 -0400

    wip

--- fresh_with_android.sh
+++ fresh_with_android.sh
@@ -27,6 +27,17 @@ npm install pm2@latest -g
 
 source $HOME/.macos
 
+brew install Python
+ln -sf $(readlink $(which python3)) $(dirname $(which python3))/python
+pip3 install pyautogui Pillow opencv-python
+
+brew install cliclick
+
+brew tap homebrew/cask-versions
+brew install android-studio-preview-beta
+
+echo "export PATH=$HOME/Library/Android/sdk/emulator:$PATH" >>~/.zshrc
+
 GREEN='\033[1;32m'
 NC='\033[0m' # No Color
 
@@ -36,4 +47,10 @@ echo -e "${GREEN}applications${NC} installed"
 echo -e "${GREEN}rendering-ios${NC} cloned"
 echo -e "${GREEN}npm dependencies${NC} installed"
 echo -e "${GREEN}pm2${NC} installed"
+echo -e "${GREEN}Python and dependencies${NC} installed"
+echo -e "${GREEN}cliclick${NC} installed"
+echo -e "${GREEN}android-studio-preview-beta${NC} installed"
+echo -e "${GREEN}emulator path${NC} exported"
 echo -e "${GREEN}✓ Done${NC}"
+echo
+echo -e "Go to ${GREEN}https://github.com/InboxMonster/rendering-ios/tree/main/android#set-up-emulator-and-app${NC}"
