e96ae5c526da91ee3f51bffdba054242ab80f86b
Keunbae Park <pkboom@users.noreply.github.com>
2024-12-18 15:44:52 -0500

    wip

--- fresh.sh
+++ fresh.sh
@@ -39,7 +39,8 @@ pip install pyautogui Pillow opencv-python
 # echo "export PATH=\"\$HOME/Library/Android/sdk/emulator:\$HOME/Library/Android/sdk/platform-tools:\$PATH\"" >>~/.zshrc
 
 # Symlink for java
-sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+# You can find this command by running `brew info openjdk`
+sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 
 GREEN='\033[1;32m'
 NC='\033[0m' # No Color
