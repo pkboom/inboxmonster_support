418ceb19368a8481cf775589c69f1647aec50ffc
Keunbae Park <pkboom@users.noreply.github.com>
2025-09-06 17:05:31 -0400

    wip

--- end.sh
+++ end.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-osascript -e 'tell application "Docker Desktop" to quit' 2>/dev/null;
-osascript -e 'tell application "Slack" to quit' 2>/dev/null
-osascript -e 'tell application "DBeaver" to quit' 2>/dev/null
-osascript -e 'tell application "iTerm2" to quit' 2>/dev/null
-osascript -e 'tell application "Visual Studio Code" to quit' 2>/dev/null
+apps=(
+  "Docker Desktop"
+  "Slack"
+  "DBeaver"
+  "iTerm2"
+  "Visual Studio Code"
+  "Clock"
+)
+
+for app in "${apps[@]}"; do
+  osascript -e "tell application \"$app\" to quit" 2>/dev/null || killall "$app" 2>/dev/null
+done
