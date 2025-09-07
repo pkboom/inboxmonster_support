69686a58befc785adacc3d8d268360516806885d
Keunbae Park <pkboom@users.noreply.github.com>
2025-09-07 11:54:28 -0400

    wip

--- end.sh
+++ end.sh
@@ -6,9 +6,17 @@ apps=(
   "DBeaver"
   "iTerm2"
   "Visual Studio Code"
-  "Clock"
 )
 
 for app in "${apps[@]}"; do
   osascript -e "tell application \"$app\" to quit" 2>/dev/null || killall "$app" 2>/dev/null
+done
+
+apps=(
+  "Clock"
+  "Spark"
+)
+
+for app in "${apps[@]}"; do
+    killall -9 "$app" 2>/dev/null
 done
